e882c1ff2898d3b2d593f8766ed71e9bd3c01439
Вячеслав <vmikolyuk@naumen.ru>
2023-06-21 10:39:07 +0300

    Инициализация спринга

---
+++ src/main/java/com/eventHive/EventHive/EventHiveApplication.java
@@ -0,0 +1,13 @@
+package com.eventHive.EventHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventHive/EventHive/EventHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventHive.EventHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
